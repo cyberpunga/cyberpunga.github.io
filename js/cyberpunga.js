f3912232a868d088c157479eed7aa5daf8cea75e
primaveraentalca <tetsuo@cyberpun.ga>
2016-06-02 02:31:05 -0400

    1st commit

---
+++ js/cyberpunga.js
@@ -0,0 +1,36 @@
+function goHome() {
+	Reveal.slide( 0, 0 );
+}
+function goDown() {
+    Reveal.down();
+}
+function goRight() {
+    Reveal.right();
+}
+
+Reveal.addEventListener( 'slidechanged', function( event ) {
+    if( Reveal.isFirstSlide() ) {
+        $('#header').css({
+            opacity: 0,
+            pointerEvents: 'none'
+        });
+    } else if( Reveal.isLastSlide() ) {
+        $('#header').css({
+            opacity: 0,
+            pointerEvents: 'none'
+        });
+    } else {
+    	$('#header').css({
+    		opacity: 1,
+            pointerEvents: 'auto',
+            cursor: 'pointer'
+    	});
+    }
+} );
+
+
+Reveal.addEventListener( 'fin', function() {
+    console.log('soy el fin.');
+}, false );
+var indices = Reveal.getIndices( document.getElementById( 'sectionID' ) );
+Reveal.slide( indices.h, indices.v );
