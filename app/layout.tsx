30c2b8f3df72c9dbb13306a9910ec684146ea662
Rodrigo Moreira <hola@piducan.dev>
2025-02-21 19:49:07 -0300

    hello world

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
-import './global.css'
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
-import { baseUrl } from './sitemap'
+import "./global.css";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Navbar } from "./components/nav";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Footer from "./components/footer";
+import { baseUrl } from "./sitemap";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'cyberpunga',
-    template: '%s | cyberpunga',
+    default: "cyberpunga",
+    template: "%s | cyberpunga",
   },
-  description: 'si estás leyendo esto, significa que sigues con vida y nos alegra muchísimo.',
+  description: "si estás leyendo esto, significa que sigues con vida y nos alegra muchísimo.",
   openGraph: {
-    title: 'cyberpunga',
-    description: 'si estás leyendo esto, significa que sigues con vida y nos alegra muchísimo.',
+    title: "cyberpunga",
+    description: "si estás leyendo esto, significa que sigues con vida y nos alegra muchísimo.",
     url: baseUrl,
-    siteName: 'cyberpunga',
-    locale: 'es_CL',
-    type: 'website',
+    siteName: "cyberpunga",
+    locale: "es_CL",
+    type: "website",
   },
   robots: {
     index: true,
@@ -29,31 +29,23 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
-}
+};
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes) => classes.filter(Boolean).join(" ");
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html
       lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-black h-full',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
+      className={cx("text-black bg-white dark:text-white dark:bg-black h-full", GeistSans.variable, GeistMono.variable)}
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto h-full">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0 h-full">
+        <main className="flex-auto flex flex-col px-2 md:px-0 h-full">
           <Navbar />
           {children}
           <Footer />
@@ -62,5 +54,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  )
+  );
 }
