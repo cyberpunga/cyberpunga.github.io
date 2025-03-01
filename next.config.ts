5bcfc4931334b41bc3fd77420c3efb4ab58678e8
Rodrigo Moreira <hola@piducan.dev>
2025-03-01 16:52:59 -0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,15 @@
-import createMDXPlugin from "@next/mdx";
+import createMDX from "@next/mdx";
 import { NextConfig } from "next";
 
-const mdxConfig = {
-  extension: /\.mdx?$/,
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-};
-
-const withMDX = createMDXPlugin(mdxConfig);
-
 const nextConfig: NextConfig = {
-  // Configure pageExtensions to include md and mdx
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
-  reactStrictMode: true,
-  output: "export",
 };
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
 
 // Merge MDX config with Next.js config
-module.exports = withMDX(nextConfig);
+export default withMDX(nextConfig);
