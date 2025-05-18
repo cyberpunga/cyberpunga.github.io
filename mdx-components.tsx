e961fc1ee71713cc4fbec30fc4fdd420ec413ed5
Rodrigo Moreira <hola@piducan.dev>
2025-05-18 07:14:55 -0400

    style posts images

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,16 @@
-import type { MDXComponents } from 'mdx/types';
-import Image from 'next/image';
+import type { MDXComponents } from "mdx/types";
+import Image from "next/image";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    img: (props) => <Image {...props} unoptimized alt={props.alt} />,
+    img: (props) => (
+      <Image
+        {...props}
+        unoptimized
+        alt={props.alt}
+        className="rounded-lg max-w-11/12 mx-auto shadow-xl border border-zinc-200 dark:border-zinc-800"
+      />
+    ),
   };
 }
